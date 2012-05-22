e50a4d1b5fb6f16cdca7175cfe68319901bf7a6d
Joshua T Corbin <jcorbin@wunjo.org>
2012-05-21 20:18:44 -0400

    Add extractor.regex.RegexExtractor

--- extractor/regex.py
+++ extractor/regex.py
@@ -1,6 +1,7 @@
 import operator
 import re
 from pyparsing import *
+from common import strtuple
 
 Regex_flag_map = dict(zip('ilmsxu', (
     re.I, re.L, re.M, re.S, re.X, re.U)))
@@ -10,3 +11,26 @@ Regex_flags.setParseAction(lambda toks: reduce(operator.or_,
 
 LiteralRegex = QuotedString('/', escChar='\\') + Regex_flags
 LiteralRegex.setParseAction(lambda toks: re.compile(*toks))
+
+class RegexExtractor(object):
+    def __init__(self, regex):
+        self.regex = regex
+
+    def __repr__(self):
+        return '%s(re.compile(%r, %d))' % (self.__class__.__name__,
+            self.regex.pattern, self.regex.flags)
+
+    def __call__(self, s):
+        if not isinstance(s, basestring): s = str(s)
+        match = self.regex.search(s)
+        if match:
+            gs = match.groups()
+            if not len(gs):
+                return match.group(0)
+            elif len(gs) == 1:
+                return gs[0]
+            else:
+                return strtuple(gs)
+
+    parse_expression = LiteralRegex.copy().setParseAction(
+        lambda toks: RegexExtractor(re.compile(*toks)))
