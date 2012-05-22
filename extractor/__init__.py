822323e05038ca05efdd610a8fdac5a33747ff54
Joshua T Corbin <jcorbin@wunjo.org>
2012-05-21 20:38:46 -0400

    Add extractor.ExtractorPipe

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -7,3 +7,24 @@ pylambda = Combine(
     Word(alphas, alphanums) + ':' +
     CharsNotIn('|'))
 pylambda.setParseAction(lambda toks: eval(toks[0]))
+
+class ExtractorPipe(object):
+    def __init__(self, extractors):
+        self.extractors = tuple(extractors)
+
+    def __repr__(self):
+        return '%s(%r)' % (
+            self.__class__.__name__,
+            self.extractors)
+
+    def __call__(self, s):
+        for extractor in self.extractors:
+            s = extractor(s)
+        return s
+
+    parse_expression = delimitedList(
+        pylambda |
+        FieldExtractor.parse_expression |
+        RegexExtractor.parse_expression,
+        delim='|')
+    parse_expression.setParseAction(lambda toks: ExtractorPipe(toks))
