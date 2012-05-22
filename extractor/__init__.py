38bb7f112a49d50423371c30bd9a436b37bf47d0
Joshua T Corbin <jcorbin@wunjo.org>
2012-05-21 20:37:00 -0400

    Add extractor.pylambda

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,2 +1,9 @@
 from regex import RegexExtractor
 from field import FieldExtractor
+from pyparsing import *
+
+pylambda = Combine(
+    'lambda' + White(' ') +
+    Word(alphas, alphanums) + ':' +
+    CharsNotIn('|'))
+pylambda.setParseAction(lambda toks: eval(toks[0]))
