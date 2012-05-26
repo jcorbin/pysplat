0f2d650c2c2d5a8047ced6d12ed30705e10ce0c4
Joshua T Corbin <jcorbin@wunjo.org>
2012-05-25 21:00:20 -0400

    Lift key -> path logic into strtuple

--- extractor/common.py
+++ extractor/common.py
@@ -1,3 +1,17 @@
+import os
+
+StrTupleNonePath = '__none__'
+
 class strtuple(tuple):
+    def path_components(self):
+        for part in self:
+            if part is None:
+                yield StrTupleNonePath
+            else:
+                yield str(part)
+
+    def topath(self):
+        return os.path.sep.join(self.path_components())
+
     def __str__(self):
         return ' '.join('' if term is None else term for term in self)
