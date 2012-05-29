820f017f42213134352fe76f6a5a8ec3e620e60d
Joshua T Corbin <jcorbin@wunjo.org>
2012-05-29 19:30:44 -0400

    Break ExtractorPipe out of __init__.py

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,23 +1,4 @@
-from pyparsing import *
-from term import ExtractorTerm
-
-class ExtractorPipe(object):
-    def __init__(self, extractors):
-        self.extractors = tuple(extractors)
-
-    def __repr__(self):
-        return '%s(%r)' % (
-            self.__class__.__name__,
-            self.extractors)
-
-    def __call__(self, s):
-        for extractor in self.extractors:
-            s = extractor(s)
-            if s is None: return None
-        return s
-
-    parse_expression = delimitedList(ExtractorTerm, delim='|')
-    parse_expression.setParseAction(lambda toks: ExtractorPipe(toks))
+from pipe import ExtractorPipe
 
 def parse(string):
     return ExtractorPipe.parse_expression.parseString(string)
---
+++ extractor/pipe.py
@@ -0,0 +1,20 @@
+from pyparsing import *
+from term import ExtractorTerm
+
+class ExtractorPipe(object):
+    def __init__(self, extractors):
+        self.extractors = tuple(extractors)
+
+    def __repr__(self):
+        return '%s(%r)' % (
+            self.__class__.__name__,
+            self.extractors)
+
+    def __call__(self, s):
+        for extractor in self.extractors:
+            s = extractor(s)
+            if s is None: return None
+        return s
+
+    parse_expression = delimitedList(ExtractorTerm, delim='|')
+    parse_expression.setParseAction(lambda toks: ExtractorPipe(toks))
