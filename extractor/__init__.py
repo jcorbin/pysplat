86ed7ecc68117c8f6acae2ff0504b30b9ccf839d
Joshua T Corbin <jcorbin@wunjo.org>
2012-05-29 22:19:07 -0400

    Add compound parsing to extractor.parse

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,4 +1,10 @@
+from compound import CompoundExtractor
 from pipe import ExtractorPipe
 
+expr = \
+    CompoundExtractor.parse_expression | \
+    ExtractorPipe.parse_expression
+expr.streamlined = True
+
 def parse(string):
-    return ExtractorPipe.parse_expression.parseString(string)
+    return expr.parseString(string)
