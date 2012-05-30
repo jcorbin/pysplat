e5fd677170ec7e8c670c493933da8069dea4e6d1
Joshua T Corbin <jcorbin@wunjo.org>
2012-05-29 22:18:39 -0400

    Add CompoundExtractor grammar

--- extractor/compound.py
+++ extractor/compound.py
@@ -1,5 +1,18 @@
 from collections import Sequence
+from pyparsing import *
+
 from common import strtuple
+from pipe import ExtractorPipe
+
+compound_term = MatchFirst([ExtractorPipe.parse_expression])
+compound_expr = Suppress('(') + \
+    delimitedList(compound_term, delim=',') + \
+    Suppress(')')
+compound_term.append(compound_expr)
+
+@compound_expr.setParseAction
+def compound_expr(toks):
+    return CompoundExtractor(toks)
 
 class CompoundExtractor(tuple):
     def extract(self, s):
@@ -8,3 +21,5 @@ class CompoundExtractor(tuple):
 
     def __call__(self, s):
         return strtuple(self.extract(s))
+
+    parse_expression = compound_expr
